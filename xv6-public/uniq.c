6bb0e93b641670d4f267fe29cc37ef4265eb5d38
rahul_ashlesh <rahulmashlesh@gmail.com>
2018-10-01 13:06:22 -0400

    Unstable release 18.9.0.0-20181001

--- xv6-public/uniq.c
+++ xv6-public/uniq.c
@@ -1,19 +1,21 @@
 //
 // Created by rahul on 9/26/18.
-
-//if ((s1[i] | 32) < (s2[i] | 32))
 //
 
 #include "types.h"
 #include "stat.h"
 #include "user.h"
 
-char buf[512];
+char buf[1024*4];
 char *str;
 int arrayOfDelimitr[512];
 int count;
+int flagc=0, flagi=0, flagd=0;
 
-void display(int index) {
+void display(int index, int occurance) {
+    if (flagc==1) {
+        printf(1,"%4d ", occurance);
+    }
     while( (str[index] != '\n') || (str[index] == str[strlen(str)]) ){
         printf(1,"%c",str[index]);
         index++;
@@ -23,21 +25,18 @@ void display(int index) {
 
 char* readFile (int fd) {
     int n;
-    char *str;
     n = read(fd, buf, sizeof(buf));
     if(n < 0) {
         printf(1, "uniq: read error\n");
         exit();
     }
-
     str = malloc(strlen(buf) + 1);
     strcpy(str,buf);
     return str;
 }
 
-
-int compare (int thisPoint, int nextPoint) {
-    int flag=0,j; //o is same , 1 is diff.
+int compare (int thisPoint, int nextPoint) {    // case sensitive
+    int flag=0,j;
     for (j = 0; j<=5; j++) {
         if(str[arrayOfDelimitr[thisPoint]+j] != str[arrayOfDelimitr[nextPoint]+j]){
             flag =1;
@@ -46,19 +45,28 @@ int compare (int thisPoint, int nextPoint) {
     return flag;
 }
 
+int icompare (int thisPoint, int nextPoint) {   // not case sensitive
+    int flag=0,j;
+    for (j = 0; j<=5; j++) {
+        if( (str[arrayOfDelimitr[thisPoint]+j] | 32) != (str[arrayOfDelimitr[nextPoint]+j] | 32)) {
+            flag =1;
+        }
+    }
+    return flag;
+}
+
+/*Get the positions of the delimiter '\n' or EOF.
+ * Returns the Number of lines in the file*/
 int parseFile () {
     arrayOfDelimitr[0]=0;
     int i, j =1;
     for (i = 0;i <= strlen(str) ;i++) {
-        if (str[i]=='\n') {
-            printf(1, "position = %d\n", i);
+        if (str[i] == str[strlen(str)]) {
             count++;
             arrayOfDelimitr[j]=i+1;
             j++;
         }
-        if (str[i] == str[strlen(str)]) {
-            printf(1, "position = %d\n", i);
-            printf(1,"!!you reached the last line!!\n");
+        if (str[i]=='\n') {
             count++;
             arrayOfDelimitr[j]=i+1;
             j++;
@@ -66,47 +74,98 @@ int parseFile () {
     }
     return count;
 }
-
-
+/*This is where the Magic! happens*/
 void uniq(int fd) {
-
     str = readFile(fd);
     count = parseFile();
     int i, occurence, j;
-
     for (i=1; i<=count;i=i+occurence) {
         occurence = 1;
-        display(arrayOfDelimitr[i - 1]);
-
-        for (j = i; j < count; j++) {
-            //printf(1,"%d,%d\n",i-1,j);
-            if (compare(i -1, j) == 0) {
-                occurence++; //true ie same
-            } else {
-                //printf(1,"Ocrn = %d, i = %d \n",occurence,i-1);
-                break;
+        if (flagd==1) {
+            /*finding the duplicates */
+            for (j = i; j <= count; j++) {
+                if (flagi == 1) {
+                    //Case-sensitive
+                    if (icompare(i - 1, j) == 0) {
+                        occurence++;
+                    } else {
+                        if (occurence>1) {
+                            display(arrayOfDelimitr[i - 1], occurence);
+                        }
+                        break;
+                    }
+                } else {
+                    //Not case-sensitive
+                    if (compare(i - 1, j) == 0) {
+                        occurence++;
+                    } else {
+                        if (occurence>1) {
+                            display(arrayOfDelimitr[i - 1], occurence);
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        /*Regular use case to print the unique*/
+        else {
+            for (j = i; j <= count; j++) {
+                if (flagi == 1) {
+                    //Case-sensitive
+                    if (icompare(i - 1, j) == 0) {
+                        occurence++;
+                    } else {
+                        display(arrayOfDelimitr[i - 1], occurence);
+                        break;
+                    }
+                } else {
+                    //Not case-sensitive
+                    if (compare(i - 1, j) == 0) {
+                        occurence++;
+                    } else {
+                        display(arrayOfDelimitr[i - 1], occurence);
+                        break;
+                    }
+                }
             }
         }
     }
 }
 
+/*command usage*/
+void usage() {
+   printf(1,"uniq [-c count,prefix lines by number of occurrences] [-d repeated,  only duplicate lines] [-i ignore-case, ignore case when comparin ]... [INPUT [OUTPUT]]\n");
+}
+
 int
 main(int argc, char *argv[]) {
-
     int fd, i;
-
     if(argc <= 1){
         uniq(0);
         exit();
     }
-
-    for(i = 1; i < argc; i++){
-        if((fd = open(argv[i], 0)) < 0){
-            printf(1, "uniq: cannot open %s\n", argv[i]);
+    for(i = 1; i < argc; i++) {
+        if (strcmp(argv[i],"-c")==0) {
+            flagc=1;
+        } else if (strcmp(argv[i],"-i")==0) {
+            flagi=1;
+        } else if (strcmp(argv[i],"-d")==0) {
+            flagd=1;
+        }
+        /*-c and -d are mutually exclusive*/
+        if ((flagc==1 ) && (flagd==1)) {
+            printf(2,"Error uniq: arguments -c and -d cant be used together\n");
+            usage();
             exit();
         }
-        uniq(fd);
-        close(fd);
     }
+    if((fd = open(argv[i-1], 0)) < 0){
+        printf(1, "uniq: cannot open %s\n", argv[i]);
+        usage();
+        exit();
+    }
+    uniq(fd);
+    close(fd);
+    free(str);  // free memory !!
     exit();
 }
